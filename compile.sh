18d7d2a93e8ccb5148ff25b940a280852797f33d
Nathan <narthur.a@gmail.com>
2015-05-27 15:40:04 -0500

    Modified compile script and ignored compiled folder

--- compile.sh
+++ compile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 mkdir compiled
-pandoc -s -f markdown -t latex -o compiled/handbook-proof.pdf Section\ 1.md
-pandoc -s -f markdown -t icml -o compiled/handbook.icml Section\ 1.md
+pandoc -s -f markdown -t latex -o compiled/handbook-proof.pdf *.md
+pandoc -s -f markdown -t icml -o compiled/handbook.icml *.md
+pandoc -s -f markdown -t markdown -o compiled/handbook.md *.md
 
 exit 0
