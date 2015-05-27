f2ac5d7b73b0c950e3e7723deaa981b4cfadf61d
Nathan <narthur.a@gmail.com>
2015-05-27 08:07:20 -0500

    Compile script added

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir compiled
-mkdir compiled/pdf
-pandoc -s -f markdown -t latex -o compiled/pdf/handbook.pdf Section\ 1.md
+pandoc -s -f markdown -t latex -o compiled/handbook-proof.pdf Section\ 1.md
+pandoc -s -f markdown -t icml -o compiled/handbook.icml Section\ 1.md
 
 exit 0
