fe9e4916880dfc4e53636ef97bca67a81ab77501
Nathan <narthur.a@gmail.com>
2015-05-28 09:40:42 -0500

    Modified compile.sh to ignore .md files not beginning with Section

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir compiled
-pandoc -s -f markdown -t latex -o compiled/handbook-proof.pdf *.md
-pandoc -s -f markdown -t icml -o compiled/handbook.icml *.md
+pandoc -s -f markdown -t latex -o compiled/handbook-proof.pdf Section*.md
+pandoc -s -f markdown -t icml -o compiled/handbook.icml Section*.md
 rm compiled/handbook.md
-for f in *.md; do (cat "${f}"; echo) >> compiled/handbook.md; done
+for f in Section*.md; do (cat "${f}"; echo) >> compiled/handbook.md; done
 
 exit 0
