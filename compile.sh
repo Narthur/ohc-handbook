d77519f6a173233c6e496a52eaf87bb3b6f07fb8
Nathan <narthur.a@gmail.com>
2015-05-29 10:54:17 -0500

    Added html compiling to compile script

--- compile.sh
+++ compile.sh
@@ -3,6 +3,7 @@
 mkdir compiled
 pandoc -s -f markdown -t latex -o compiled/handbook-proof.pdf Section*.md
 pandoc -s -f markdown -t icml -o compiled/handbook.icml Section*.md
+pandoc -s -f markdown -t html -o compiled/handbook.html Section*.md
 rm compiled/handbook.md
 for f in Section*.md; do (cat "${f}"; echo) >> compiled/handbook.md; done
 
